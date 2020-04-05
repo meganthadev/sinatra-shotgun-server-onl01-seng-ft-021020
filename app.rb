4a55e13cf04f33775cb2fd55e5330642d7350a7a
Megan Horsfield <meganthadev@gmail.com>
2020-04-05 06:00:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,14 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! I BUILT THIS!"
+    "Started my server using Shotgun!"
   end
 
 end
 
-ENV['SINATRA_ENV'] ||= "development"
-
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-require './app'
